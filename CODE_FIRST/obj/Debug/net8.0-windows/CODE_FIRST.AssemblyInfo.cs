a7dc7d61020fdc3fd003477b554d976d004e9bcc
jaumemontilivi <a2208027@institutmontilivi.cat>
2024-03-03 19:46:07 +0100

    versió4

--- CODE_FIRST/obj/Debug/net8.0-windows/CODE_FIRST.AssemblyInfo.cs
+++ CODE_FIRST/obj/Debug/net8.0-windows/CODE_FIRST.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CODE_FIRST")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+423d6ec2695a77a0074d037ceb666ab36b859f5c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec83580027ccd7ce01c31c2f3dde81a9fda6c041")]
 [assembly: System.Reflection.AssemblyProductAttribute("CODE_FIRST")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CODE_FIRST")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
